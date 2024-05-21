7b8f14b75717b59f2221b8b1d31a7c4c6ae0489a
ahma0 <dsd932@naver.com>
2024-05-21 21:56:23 +0900

    feat: barchart 생성 코드 추가

--- src/main/kotlin/kt/toyproject/gitgitlevel/controller/ChartController.kt
+++ src/main/kotlin/kt/toyproject/gitgitlevel/controller/ChartController.kt
@@ -2,10 +2,15 @@ package kt.toyproject.gitgitlevel.controller
 
 import kt.toyproject.gitgitlevel.graphql.data.ContributionCalendarDay
 import kt.toyproject.gitgitlevel.service.ChartService
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.io.FileInputStream
 
 @RestController
 @RequestMapping("/chart")
@@ -16,5 +21,16 @@ class ChartController(
     @GetMapping
     fun getChartImage(@RequestParam(name = "username") githubId: String) : MutableMap<Int, List<ContributionCalendarDay>> =
         chartService.getChartImage(githubId)
+
+    @GetMapping("/barchart")
+    fun generateChartImage(): ResponseEntity<InputStreamResource> {
+        val chartFile = chartService.generateChartImage()
+        val resource = InputStreamResource(FileInputStream(chartFile))
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"barchart.png\"")
+            .contentType(MediaType.IMAGE_PNG)
+            .body(resource)
+    }
 
 }
--- src/main/kotlin/kt/toyproject/gitgitlevel/service/ChartService.kt
+++ src/main/kotlin/kt/toyproject/gitgitlevel/service/ChartService.kt
@@ -1,9 +1,13 @@
 package kt.toyproject.gitgitlevel.service
 
-import kt.toyproject.gitgitlevel.graphql.ContributionCalendarDay
 import kt.toyproject.gitgitlevel.graphql.GithubApi
 import kt.toyproject.gitgitlevel.graphql.data.ContributionCalendarDay
+import org.jfree.chart.ChartFactory
+import org.jfree.chart.ChartUtils
+import org.jfree.chart.JFreeChart
+import org.jfree.data.category.DefaultCategoryDataset
 import org.springframework.stereotype.Service
+import java.io.File
 
 @Service
 class ChartService(
@@ -16,6 +20,27 @@ class ChartService(
         return githubApi.getContributionLevel(githubId, contributionYears)
 
 //        return githubApi.getTotalContributionCount(githubId, contributionYears)
+    }
+
+    fun generateChartImage(): File {
+        val dataset = DefaultCategoryDataset().apply {
+            addValue(1.0, "Series1", "Category1")
+            addValue(4.0, "Series1", "Category2")
+            addValue(3.0, "Series1", "Category3")
+        }
+
+        // 차트 생성
+        val chart: JFreeChart = ChartFactory.createBarChart(
+            "Sample Bar Chart",  // 차트 제목
+            "Category",          // X축 라벨
+            "Value",             // Y축 라벨
+            dataset
+        )
+
+        // 차트를 이미지 파일로 저장
+        val chartFile = File("barchart.png")
+        ChartUtils.saveChartAsPNG(chartFile, chart, 800, 600)
+        return chartFile
     }
 
 }
