0a1fa84c69d034ab89eadc232aeb65fae702a748
ahma0 <dsd932@naver.com>
2024-05-27 22:45:24 +0900

    feat: ts에서 Base64를 받아서 디코딩하여 이미지로 변환하는 로직 구현

--- src/main/kotlin/kt/toyproject/gitgitlevel/script/ScriptRunner.kt
+++ src/main/kotlin/kt/toyproject/gitgitlevel/script/ScriptRunner.kt
@@ -1,24 +1,52 @@
 package kt.toyproject.gitgitlevel.script
 
 import com.google.gson.Gson
+import io.micrometer.core.instrument.util.StringEscapeUtils
 import kt.toyproject.gitgitlevel.data.ChartTypes
 import kt.toyproject.gitgitlevel.graphql.data.ContributionCalendarDay
 import org.springframework.stereotype.Component
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.awt.image.BufferedImage
+import java.io.*
+import java.util.*
+import javax.imageio.ImageIO
 
 @Component
 class ScriptRunner {
     fun runScript(
-        contributionCountByYear:  MutableMap<Int, Int>,
+        contributionCountByYear: MutableMap<Int, Int>,
         chartTypes: ChartTypes
-    ): String {
+    ): ByteArray {
         val toJson = Gson().toJson(contributionCountByYear)
+        val escapedJson = StringEscapeUtils.escapeJson(toJson)
+        val command = "npx ts-node src/main/resources/scripts/index.ts $escapedJson, $chartTypes"
+        val process = Runtime.getRuntime().exec(command)
 
-        val command = "npx ts-node ./src/main/resources/scripts/index.ts '$toJson', '$chartTypes'"
-        val process = ProcessBuilder(*command.split(" ").toTypedArray()).start()
-        val reader = BufferedReader(InputStreamReader(process.inputStream))
-        return reader.toString()
+        // 프로세스의 출력 스트림을 읽어오기 위한 BufferedReader 생성
+        val reader = process.inputStream.bufferedReader()
+
+        val output = StringBuilder()
+
+        // BufferedReader를 통해 프로세스의 출력을 읽어옴
+        val lines = reader.readLines()
+        for (line in lines) {
+            output.append(line)
+        }
+
+        // 프로세스가 종료될 때까지 대기
+        process.waitFor()
+
+
+        // TypeScript 스크립트로부터 반환된 base64 이미지 데이터를 디코딩하여 이미지로 변환
+//        val imageBytes = Base64.getDecoder().decode(output.toString())
+        val base64ImageSafe = output.toString().replace('-', '+').replace('_', '/')
+        val imageBytes = Base64.getDecoder().decode(base64ImageSafe)
+        val imageStream = ByteArrayInputStream(imageBytes)
+        val bufferedImage = ImageIO.read(imageStream)
+
+        val outputStream = ByteArrayOutputStream()
+        ImageIO.write(bufferedImage, "png", outputStream)
+
+        return outputStream.toByteArray()
     }
 
 }
