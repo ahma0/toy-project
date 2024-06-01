3d80c47691e71c769d290dd945a65c18fa50021f
ahma0 <dsd932@naver.com>
2024-06-01 23:35:55 +0900

    feat: 차트의 타입에 따라 함수를 실행하는 로직 추가

--- src/main/resources/scripts/index.ts
+++ src/main/resources/scripts/index.ts
@@ -1,15 +1,33 @@
 import {createBarChart} from './charts/barChart';
+import {ChartTypes, ContributionData} from './data/ContributionDatas';
+import {createPieChart} from "./charts/pieChart";
 
-export function makeChart(contributionDataJson: string, chartTypes: ChartTypes): string {
-    const contributionData: ContributionData = JSON.parse(contributionDataJson);
-    return typeof contributionData
+function makeChart(contributionDataJson: string, chartTypes: ChartTypes): Promise<string> {
+    let result: Promise<string> = Promise.resolve("initial value");
+    switch (chartTypes) {
+        case ChartTypes.BAR:
+            const contributionData: ContributionData = JSON.parse(contributionDataJson);
+            result = createBarChart(contributionData);
+            break;
+        case ChartTypes.PIE:
+            // JSON 파싱
+            const jsonData = JSON.parse(contributionDataJson);
+            result = createPieChart(jsonData);
+            break;
+    }
+    return result
 }
 
-
-interface ContributionData {
-    [year: number]: number;
+// main 함수 정의
+if (process.argv.length < 4) {
+    console.error("Usage: npx ts-node src/main/resources/scripts/index.ts <contributionDataJson> <chartType>");
+    process.exit(1);
 }
 
-enum ChartTypes {
-    BAR, PIE, CANDLE_STICK, BAR_3D
-}
+// Kotlin에서 전달된 데이터
+const contributionDataJson: string = process.argv[2];
+const chartTypes: ChartTypes = process.argv[3] as ChartTypes;
+
+// makeChart 함수 호출
+const result: Promise<string> = makeChart(contributionDataJson, chartTypes);
+console.log(result);
