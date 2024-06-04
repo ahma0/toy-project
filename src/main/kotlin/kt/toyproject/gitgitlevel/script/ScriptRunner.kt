347c7a8e5a5a31d974401e86a4880a625c7d28b0
ahma0 <dsd932@naver.com>
2024-06-04 21:49:04 +0900

    feat: ts 실행 메소드 수정

--- src/main/kotlin/kt/toyproject/gitgitlevel/script/ScriptRunner.kt
+++ src/main/kotlin/kt/toyproject/gitgitlevel/script/ScriptRunner.kt
@@ -1,24 +1,22 @@
 package kt.toyproject.gitgitlevel.script
 
 import com.google.gson.Gson
-import io.micrometer.core.instrument.util.StringEscapeUtils
 import kt.toyproject.gitgitlevel.data.ChartTypes
-import kt.toyproject.gitgitlevel.graphql.data.ContributionCalendarDay
 import org.springframework.stereotype.Component
-import java.awt.image.BufferedImage
 import java.io.*
 import java.util.*
 import javax.imageio.ImageIO
 
 @Component
 class ScriptRunner {
+
     fun runScript(
         contributionCountByYear: MutableMap<Int, Int>,
         chartTypes: ChartTypes
     ): ByteArray {
         val toJson = Gson().toJson(contributionCountByYear)
-        val escapedJson = StringEscapeUtils.escapeJson(toJson)
-        val command = "npx ts-node src/main/resources/scripts/index.ts $escapedJson, $chartTypes"
+        val filePath = getResourceFilePath("scripts/index.ts")
+        val command = "npx ts-node $filePath $toJson $chartTypes"
         val process = Runtime.getRuntime().exec(command)
 
         // 프로세스의 출력 스트림을 읽어오기 위한 BufferedReader 생성
@@ -35,18 +33,18 @@ class ScriptRunner {
         // 프로세스가 종료될 때까지 대기
         process.waitFor()
 
-
         // TypeScript 스크립트로부터 반환된 base64 이미지 데이터를 디코딩하여 이미지로 변환
-//        val imageBytes = Base64.getDecoder().decode(output.toString())
-        val base64ImageSafe = output.toString().replace('-', '+').replace('_', '/')
-        val imageBytes = Base64.getDecoder().decode(base64ImageSafe)
+        val imageBytes = Base64.getDecoder().decode(output.toString().split(",")[1].replace("'}", ""))
+
         val imageStream = ByteArrayInputStream(imageBytes)
         val bufferedImage = ImageIO.read(imageStream)
-
         val outputStream = ByteArrayOutputStream()
         ImageIO.write(bufferedImage, "png", outputStream)
-
         return outputStream.toByteArray()
+    }
+
+    private fun getResourceFilePath(fileName: String): String? {
+        return Thread.currentThread().contextClassLoader.getResource(fileName)?.path
     }
 
 }
